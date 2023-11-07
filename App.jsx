0a02fba6252865b44860e3644d490d2037c381ed
Beatrizmartins15 <beatrizmarti3153@gmail.com>
2023-11-07 09:04:53 -0300

    Aula 07/11

--- App.jsx
+++ App.jsx
@@ -2,18 +2,26 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
-import Cadastrar from './components/cadastrar';
+import { Cadastrar } from './components/cadastrar';
 import Listar from './components/listar';
 import { Ionicons } from '@expo/vector-icons';
 import { FontAwesome } from '@expo/vector-icons';
 import { Entypo } from '@expo/vector-icons';
+import Parse from "parse/react-native.js";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+
+//Initializing the SDK. 
+Parse.setAsyncStorage(AsyncStorage);
+//You need to copy BOTH the the Application ID and the Javascript Key from: Dashboard->App Settings->Security & Keys 
+Parse.initialize('5LY9Kh6n6IYw61L2R6dcMu5trC60nRr0L79xNy3H','TwFbOzOPFyyyhduZ2SVOqolB6vArRW2MjLGaqz0q');
+Parse.serverURL = 'https://parseapi.back4app.com/';
 
 const Tabs = createBottomTabNavigator();
 export default function App() {
   return (
   <NavigationContainer>
-    <Tabs.Navigator screenOptions={{ headerStyle:{ backgroundColor: "#00f" }, headerTitleStyle:{ color: "#FFF", fontWeight:"100"}, headerTitleAlign: "center", tabBarStyle:{ height: "60", backgroundColor:"#000" }}}>
+    <Tabs.Navigator screenOptions={{ headerStyle:{ backgroundColor: "#8F00DB" }, headerTitleStyle:{ color: "#FFF", fontWeight:"100"}, headerTitleAlign: "center"}}>
       <Tabs.Screen name='Cadastro' component={Cadastrar} options={{tabBarIcon:({color, size})=>(<FontAwesome name="sign-in" size={size} color={color}/>)}}/>
       <Tabs.Screen name='Listar' component={Listar} options={{tabBarIcon:({color, size})=>(<Entypo name="list" size={size} color={color}/>)}}/>
     </Tabs.Navigator>
--- components/cadastrar/index.jsx
+++ components/cadastrar/index.jsx
@@ -1,20 +1,105 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { FC, ReactElement, useState } from "react";
+import { 
+  StyleSheet, 
+  View, 
+  Text, 
+  SafeAreaView, 
+  TextInput, 
+  Pressable, 
+  Alert 
+} from 'react-native';
+import Parse from "parse/react-native";
+import 'react-native-get-random-values';
 
-export default function Cadastrar() {
-return (
-    <View style={styles.container}>
-    <Text>Cadastrar</Text>
-    <StatusBar style="auto" />
-    </View>
-);
-}
+
+export const Cadastrar = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const doCadastrar = async function () {
+    // Note that these values come from state variables that we've declared before
+    const usernameValue = username;
+    const passwordValue = password;
+    // Since the signUp method returns a Promise, we need to call it using await
+    return await Parse.User.signUp(usernameValue, passwordValue)
+      .then((createdUser) => {
+        // Parse.User.signUp returns the already created ParseUser object if successful
+        Alert.alert(
+          "Success!",
+          `User ${createdUser.get("username")} was successfully created!`
+        );
+        return true;
+      })
+      .catch((error) => {
+        // signUp can fail if any parameter is blank or failed an uniqueness check on the server
+        Alert.alert("Error!", error.message);
+        return false;
+      });
+  };
+
+  return (
+    <SafeAreaView style={styles.container__safe}>
+        <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        value={username}
+        placeholder={"Nome do Produto"}
+        onChangeText={(text) => setUsername(text)}
+        autoCapitalize={"none"}
+      />
+      <TextInput
+        style={styles.input2}    
+        value={password}
+        placeholder={"Valor R$0,00"}
+        secureTextEntry
+        keyboardType={"numeric"}
+        onChangeText={(text) => setPassword(text)}
+      />
+      <TextInput
+        style={styles.input3}    
+        value={password}
+        placeholder={'Quantidade'}
+        secureTextEntry
+        onChangeText={(text) => setPassword(text)}
+      />
+      <Pressable title={"Sign Up"} onPress={() => doCadastrar()} />
+      </View>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-},
+  input: {
+    height: 40,
+    width: "80%",
+    marginBottom: 10,
+    backgroundColor: "#e8e8e8",
+    padding: 5,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  input2: {
+    height: 40,
+    width: "80%",
+    marginBottom: 10,
+    backgroundColor: "#e8e8e8",
+    padding: 5,
+
+  },
+  input3: {
+    height: 40,
+    width: "80%",
+    marginBottom: 10,
+    backgroundColor: "#e8e8e8",
+    padding: 5,
+  },
+    container__safe:{
+        flex: 1
+      },
+      container: {
+        flex: 1,
+        backgroundColor: '#AD95D9',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }, 
 });
